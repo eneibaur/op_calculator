d57581937a9820549d131bece92afbdb36a1c7d1
eneibaur <107266929+eneibaur@users.noreply.github.com>
2023-01-01 08:09:27 -0800

    Create event listeners for buttons

---
+++ script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('.button');
+
+const currentNumber = ""
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.innerText);
+    });
+});
+
+
+// add event listener to numbers buttons
+// user types in number, it shows up in input field. when they click on
+// an operator, that number is captured and displayed as the result.
+// So the function will 'add' each new number.
+
+// user clicks button
+// added to current string in input field
+// when user clicks operand, capture the thing in the input field
+// additionally, store operator in temp variable so I know what we're doing
+// with the next number?
+// repeat add infinitum
